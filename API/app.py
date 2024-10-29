f6be72413a6d445885733376c017bcf7034c68b6
YuriAyres <134561873+YuriAyres@users.noreply.github.com>
2024-10-29 19:37:56 -0300

    Primeiro commit api estacionamento

---
+++ API/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Configuração da URI de conexão com o banco de dados PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/carros_estacionamento'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Carro(db.Model):
+    __tablename__ = 'carros'
+    carro_id = db.Column(db.Integer, primary_key=True)
+    placa = db.Column(db.String(20), unique=True, nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.Text, nullable=True)
+
+@app.route('/estacionar', methods=['POST'])
+def carro_estacionar():
+    dados = request.json
+    carro_id = dados.get('carro_id')
+    placa = dados.get('placa')
+    
+    carro = Carro.query.get(carro_id)
+    
+    if carro:
+        if carro.status == 'estacionado':
+            return jsonify({"message": "Carro já está estacionado!"}), 400
+        carro.status = 'estacionado'  # Atualiza o status para 'estacionado'
+    db.session.commit()
+
+@app.route('/sair', methods=['POST'])
+def carro_sair():
+    dados = request.json
+    carro_id = dados.get('carro_id')
+    placa = dados.get('placa')
+    
+    carro = Carro.query.get(carro_id)
+    
+    if carro:
+        if carro.status == 'estacionado':
+            carro.status = ''  # Atualiza o status para vazio
+        else:
+            return jsonify({"message": "Carro não registrou entrada!"}), 400
+    db.session.commit()
+
+@app.route('/carros', methods=['GET'])
+def get_carros():
+    # Faz a consulta para obter todos os carros do banco de dados
+    carros = Carro.query.all()
+
+    # Transforma o resultado da consulta em um formato JSON
+    carros_list = [{'placa': carro.placa, 'nome': carro.nome, 'status': carro.status} for carro in carros]
+    return jsonify(carros_list), 200
+
+@app.route('/carros/<tag>', methods=['GET'])
+def get_carro(tag):
+    carro = Carro.query.filter_by(carro_id=tag).first()
+    if carro is None:
+        return jsonify({"message": "Carro não encontrado."}), 404
+    return jsonify({"placa": carro.placa, "nome": carro.nome}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
