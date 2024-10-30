b2b5aeb4d22758b33867d7fb6b585c3ddb4aa621
YuriAyres <yuriadp7@gmail.com>
2024-10-30 16:10:11 -0300

    Atualização API reservas e correções de parâmetros

--- API/app.py
+++ API/app.py
@@ -16,12 +16,20 @@ class Carro(db.Model):
     placa = db.Column(db.String(20), unique=True, nullable=False)
     nome = db.Column(db.String(100), nullable=False)
     status = db.Column(db.Text, nullable=True)
+    reserva = db.Column(db.Text, nullable=True)
+
+@app.route('/reservar', methods=['POST'])
+def reservar_carro():
+    dados = request.json
+    carro = Carro.query.filter_by(placa=dados['placa']).first()
+    if carro:
+        carro.reserva = dados['reserva']
+    db.session.commit()
 
 @app.route('/estacionar', methods=['POST'])
 def carro_estacionar():
     dados = request.json
     carro_id = dados.get('carro_id')
-    placa = dados.get('placa')
     
     carro = Carro.query.get(carro_id)
     
@@ -35,13 +43,13 @@ def carro_estacionar():
 def carro_sair():
     dados = request.json
     carro_id = dados.get('carro_id')
-    placa = dados.get('placa')
     
     carro = Carro.query.get(carro_id)
     
     if carro:
         if carro.status == 'estacionado':
             carro.status = ''  # Atualiza o status para vazio
+            carro.reserva = '' # Atualiza a reserva para vazia
         else:
             return jsonify({"message": "Carro não registrou entrada!"}), 400
     db.session.commit()
@@ -52,7 +60,7 @@ def get_carros():
     carros = Carro.query.all()
 
     # Transforma o resultado da consulta em um formato JSON
-    carros_list = [{'placa': carro.placa, 'nome': carro.nome, 'status': carro.status} for carro in carros]
+    carros_list = [{'placa': carro.placa, 'nome': carro.nome, 'status': carro.status, 'reserva': carro.reserva} for carro in carros]
     return jsonify(carros_list), 200
 
 @app.route('/carros/<tag>', methods=['GET'])
@@ -60,7 +68,7 @@ def get_carro(tag):
     carro = Carro.query.filter_by(carro_id=tag).first()
     if carro is None:
         return jsonify({"message": "Carro não encontrado."}), 404
-    return jsonify({"placa": carro.placa, "nome": carro.nome}), 200
+    return jsonify({"placa": carro.placa, "nome": carro.nome, "Status": carro.status, "reserva": carro.reserva}), 200
 
 
 if __name__ == '__main__':
